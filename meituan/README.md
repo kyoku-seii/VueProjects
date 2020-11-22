d46955be37a071fee63729787faefb3703873f4b
Administrator <admin@example.com>
2020-11-22 23:25:38 +0800

    删除上一个项目

--- meituan/README.md
+++
@@ -1,3 +0,0 @@
-## 仿美团外卖APP
-
-![商家页](https://raw.githubusercontent.com/xmanv/VueProjects/master/meituan/redeme.png)
--- meituan/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer: {
-        host: '0.0.0.0',
-        port: '8090',
-        open: true
-    },
-    lintOnSave: process.env.NODE_ENV !== 'production'
-}
